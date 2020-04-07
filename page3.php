573dcae3e5fbada5526924ed9e03e6c663b41218
Kostya Sarantsev <cool.olo124@list.ru>
2020-04-07 16:11:54 +0300

    first commit

---
+++ page3.php
@@ -0,0 +1,73 @@
+
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Авторизация</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="body" vlink="black">
+
+
+<nav>
+	<div class="pages">
+    
+  
+	<div class="nav"> 
+       <a href="index.html">Главная</a>
+	</div>
+	<div class="nav"> 
+       <a href="page2.html">О себе</a>
+	</div>
+
+	<div class="here nav">
+		Авторизация
+	</div>
+	
+	<div class="nav"> 
+       <a href="page4.html">Игра</a>   
+	</div>
+	<div class="nav"> 
+       <a href="page5.html">Галлерея</a>   
+	</div>
+    </div>
+</nav>
+ 
+
+
+<section>
+	<div class="section">
+       
+       <form>
+       <div class="regestration"><form action="" method="post" name="registerform"> 
+       <h1>Вход</h1><br>
+       <p><label>Введите логин:</p><br>
+       <input name="login" size="20" type="text"></label></p><br>
+       <p><label>Введите пароль:</p><br>
+       <input name="password" size="20" type="password"></label></p><br>
+       <p><input name="ok" type= "submit" value="login"></p><br>
+       </form>
+       <p>Еще не зарегистрирован?</p><br>
+       <a href=regpage.html> Зарегистрируйся </a><br>
+       </div>
+      </form>
+
+
+
+
+    </div>
+
+
+
+
+</section>
+
+
+
+
+
+</body>
+
+
+
+</html>
